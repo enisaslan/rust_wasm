19ba5491ad77879a7f18d1c7b978871b789bf497
Enis ASLAN <eenis.aslan@gmail.com>
2024-03-24 14:31:12 +0300

    button created

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
+use std::slice::Windows;
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::{closure::Closure, JsValue};
+use web_sys::{Document, MouseEvent, Window};
 
 #[wasm_bindgen]
 extern "C" {
@@ -8,4 +11,25 @@ extern "C" {
 #[wasm_bindgen]
 pub fn greet(name: &str) {
     alert(&format!("Hello, {}!", name));
+}
+
+#[wasm_bindgen(start)]
+fn run() -> Result<(), JsValue> {
+    // Use `web_sys`'s global `window` function to get a handle on the global
+    // window object.
+    let window = web_sys::window().expect("no global `window` exists");
+    let document = window.document().expect("should have a document on window");
+    let body = document.body().expect("document should have a body");
+
+    // Manufacture the element we're gonna append
+    let val = document.create_element("p")?;
+    let button = document.create_element("button")?;
+
+    val.set_text_content(Some("Hello from Rust!"));
+    button.set_text_content(Some("Click Me !"));
+
+    body.append_child(&val)?;
+    body.append_child(&button)?;
+
+    Ok(())
 }
